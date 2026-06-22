6b11c4c9e7595aedabf33e07fbcd0c2244d57dd9
kaaryancr7 <kaaryancr7@gmail.com>
2026-06-22 17:15:28 +0000

    new files

---
+++ day_20/79.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a[10][10], rows, cols;
+
+    cout << "Enter rows and columns: ";
+    cin >> rows >> cols;
+
+    cout << "Enter matrix elements:\n";
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < cols; j++)
+            cin >> a[i][j];
+
+    for (int i = 0; i < rows; i++) {
+        int sum = 0;
+        for (int j = 0; j < cols; j++) {
+            sum += a[i][j];
+        }
+        cout << "Sum of row " << i + 1 << " = " << sum << endl;
+    }
+    return 0;
+}
---
+++ day_21/82.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string str, rev = "";
+
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    for (int i = str.length() - 1; i >= 0; i--) {
+       rev += str[i];
+    }
+    cout << "Reversed string: " << rev;
+    return 0;
+}
